bbc830fcb228e3159a3cc7bea966a34246ddd2ef
NaffodA <edmondhl004@gmail.com>
2025-07-31 15:43:39 -0400

    codepath implementations and glide

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,11 @@ android {
 }
 
 dependencies {
-
+//    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+//    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.codepath.libraries:asynchttpclient:2.2.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
